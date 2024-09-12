0e4657c3ee2e5130a41ee72eb19555d7a9cb1dbe
amansgaian <aman.s@mobiusdtaas.ai>
2024-09-12 12:04:31 +0530

    removed comments

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config()
-console.log(process.env.PORT)
 const PORT  = process.env.PORT || 3000;
 const connectDB = require('./config/connection');
 connectDB();
